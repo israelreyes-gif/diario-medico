c21fa134aa1c88e56acba1445512e4eeb259cb0c
israelreyes-gif <israel.reyes@gmail.com>
2026-08-17 23:39:24 +0200

    Update home.js

--- app/js/home.js
+++ app/js/home.js
@@ -5,7 +5,7 @@
 const HOME_SECTIONS = [
   {
     className: 'meds',
-    title: 'Pastillero',
+    title: 'Medicación',
     subtitle: 'Medicación, dosis e histórico',
     icon: '<svg viewBox="0 0 24 24" fill="none" stroke="white" stroke-width="1.8" stroke-linecap="round" stroke-linejoin="round"><path d="m10.5 20.5 10-10a4.95 4.95 0 1 0-7-7l-10 10a4.95 4.95 0 1 0 7 7Z"/><path d="m8.5 8.5 7 7"/></svg>',
     onClick: 'goToMeds()',
